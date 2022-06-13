47d249d3582a8a1d6ef378282713182e9911d13a
gabrielamanoel <gabrielamanoel@ciandt.com>
2022-06-13 15:26:55 -0300

    g

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE CLIENTE (
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+nome varchar(80) NOT NULL,
+email varchar(80) NOT NULL,
+telefone int);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
