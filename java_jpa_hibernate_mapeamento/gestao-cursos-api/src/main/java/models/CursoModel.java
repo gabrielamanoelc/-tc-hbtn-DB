ceed70d059eeba10d2358c35f3d3e1d54ff03a6e
gabrielamanoel <gabrielamanoel@ciandt.com>
2022-06-26 13:15:48 -0300

    g

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/CursoModel.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/CursoModel.java
@@ -1,84 +1,81 @@
 package models;
 
-import entities.Aluno;
+import entities.Curso;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
 
-public class AlunoModel {
-
-    public void create(Aluno aluno) {
+public class CursoModel {
+    public void create(Curso curso) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
         EntityManager em = emf.createEntityManager();
 
         try {
             System.out.println("Iniciando a transação");
             em.getTransaction().begin();
-            em.merge(aluno);
+            em.merge(curso);
             em.getTransaction().commit();
-            System.out.println("Aluno criado com sucesso !!!");
+            System.out.println("Curso criado com sucesso !!!");
         } catch (Exception e) {
             em.close();
-            System.err.println("Erro ao criar um aluno !!!" + e.getMessage());
+            System.err.println("Erro ao criar um curso !!!" + e.getMessage());
         } finally {
             em.close();
             System.out.println("Finalizando a transação");
         }
     }
 
-    public Aluno findById(Long id) {
+    public Curso findById(Long id) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
         EntityManager em = emf.createEntityManager();
-        Aluno aluno = em.find(Aluno.class, id);
+        Curso curso = em.find(Curso.class, id);
         em.close();
         emf.close();
-        return aluno;
+        return curso;
     }
 
     @SuppressWarnings("unchecked")
-    public List<Aluno> findAll() {
+    public List<Curso> findAll() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
         EntityManager em = emf.createEntityManager();
 
-        List<Aluno> alunos;
+        List<Curso> cursos;
 
-        alunos = em.createQuery("FROM " + Aluno.class.getName()).getResultList();
+        cursos = em.createQuery("FROM " + Curso.class.getName()).getResultList();
         em.close();
         emf.close();
-        return alunos;
-
+        return cursos;
     }
 
-    public void update(Aluno aluno) {
+    public void update(Curso curso) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
         EntityManager em = emf.createEntityManager();
 
-        Aluno alunoUpdate = em.find(Aluno.class, aluno.getId());
+        Curso cursoUpdate = em.find(Curso.class, curso.getId());
 
         em.getTransaction().begin();
-        alunoUpdate.setNomeCompleto(aluno.getNomeCompleto());
-        alunoUpdate.setMatricula(aluno.getMatricula());
-        alunoUpdate.setNascimento(aluno.getNascimento());
-        alunoUpdate.setEmail(aluno.getEmail());
-        alunoUpdate.setEnderecos(aluno.getEnderecos());
-        alunoUpdate.setTelefones(aluno.getTelefones());
+        cursoUpdate.setNome(curso.getNome());
+        cursoUpdate.setSigla(curso.getSigla());
+        cursoUpdate.setAlunos(curso.getAlunos());
+        cursoUpdate.setProfessor(curso.getProfessor());
+        cursoUpdate.setMaterialCurso(curso.getMaterialCurso());
         em.getTransaction().commit();
 
         em.close();
         emf.close();
     }
 
-    public void delete(Aluno aluno) {
+    public void delete(Curso curso) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
         EntityManager em = emf.createEntityManager();
 
-        Aluno alunoDelete = em.find(Aluno.class, aluno.getId());
+        Curso cursoDelete = em.find(Curso.class, curso.getId());
 
 
         em.getTransaction().begin();
-        em.remove(alunoDelete);
+        em.remove(cursoDelete);
         em.getTransaction().commit();
 
         em.close();
